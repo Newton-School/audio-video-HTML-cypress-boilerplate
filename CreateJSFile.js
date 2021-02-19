fab9aaaf6e1647eb495a9614550d53cf669e30f5
Nightrider0098 <hitesh08011999@gmail.com>
2021-02-19 08:22:48 +0530

    uploaded public files and dependencies

--- CreateJSFile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-let data = fs.readFileSync('./index.html')
-data = data.toString()
-let start = data.search('<script type="text/babel">') + 26;
-let stop = data.search('</script>\n</body>')
-fs.writeFile('index.js', data.substr(start, stop - start), () => { })
